a3f54ccc98c7be8be7b1fe93f27dad57f47470b7
Ayasalem12 <aya446085@gmail.com>
2025-04-30 18:01:13 +0300

    Add layouts

--- front-end/src/app/shared/components/header/header/header.component.ts
+++ front-end/src/app/shared/components/header/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import {MainLayoutComponent}  from '../../../../layout/main-layout/main-layout/main-layout.component'
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MainLayoutComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+    img:string = '../../../../../../public/assests/images/logo.jpeg'
 }
