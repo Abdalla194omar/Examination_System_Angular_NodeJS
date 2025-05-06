fc2dcec63f4a84b613fbd7e370be54849f070d2c
Abdalla194omar <ao268314@gmail.com>
2025-05-07 02:00:35 +0300

    com

---
+++ front-end/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, UrlTree, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service'; // adjust path as needed
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean | UrlTree {
+    return this.router.createUrlTree(['/login']);
+  }
+}
